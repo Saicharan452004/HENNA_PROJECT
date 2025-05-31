57added8164d618b30b0c8f78c49e56b0aaa6a3b
saicharan <saicharan4542@gmail.com>
2025-05-31 12:33:05 +0530

    solution

--- backend/routes/faqRoutes.js
+++ backend/routes/faqRoutes.js
@@ -5,9 +5,6 @@ const faqController = require('../controllers/faqController');
 // POST route for submitting FAQ
 router.post('/', faqController.submitFAQ);
 
-router.get('/all', async (req, res) => {
-  const faqs = await faqController.find();
-  res.json(faqs);
-});
+router.get('/all', faqController.getAllFAQs);
 
 module.exports = router;
