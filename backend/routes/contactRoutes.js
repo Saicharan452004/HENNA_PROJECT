792395d021f94c69acf08ac8a877061f51d13817
saicharan <saicharan4542@gmail.com>
2025-05-31 10:47:23 +0530

    solution

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -5,6 +5,6 @@ const formController = require('../controllers/formController');
 // POST route for contact form
 router.post('/contact', formController.submitContactForm);
 
-router.get('/all', contactController.getAllContacts);
+router.get('/all', formController.getAllContacts);
 
 module.exports = router;
